9c9d651c94f7b078fa0aca6bfe2b401668bcf894
XQChen <XinquanChen0117@163.com>
2022-03-17 20:14:14 +0800

    add hessian

--- src/function.c
+++ src/function.c
@@ -49,13 +49,13 @@ void HessianMatrix(NdsclaFunction *function, const Vector *x0, double h, Vector 
             VectorCopy(x0, temp);
             temp->entry[i] += h;
             temp->entry[j] += h;
-            f_ah1_ah2 = NdsclaFunctionCall(function->function, temp);
+            f_ah1_ah2 = NdsclaFunctionCall(function, temp);
             temp->entry[j] -= 2 * h;
-            f_ah1_sh2 = NdsclaFunctionCall(function->function, temp);
+            f_ah1_sh2 = NdsclaFunctionCall(function, temp);
             temp->entry[i] -= 2 * h;
-            f_sh1_sh2 = NdsclaFunctionCall(function->function, temp);
+            f_sh1_sh2 = NdsclaFunctionCall(function, temp);
             temp->entry[j] += 2 * h;
-            f_sh1_ah2 = NdsclaFunctionCall(function->function, temp);
+            f_sh1_ah2 = NdsclaFunctionCall(function, temp);
             hessian->entry[function->inputSize * i + j] = (f_ah1_ah2 - f_ah1_sh2 - f_sh1_ah2 + f_sh1_sh2) / (4*h*h);
         }
     }
